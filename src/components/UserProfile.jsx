bd88c594f7467ff650ed64660113b1779e89b6c2
leor8 <leo442183205@gmail.com>
2018-08-02 18:57:43 -0700

    fixing merge error where codes got overwritten

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -10,8 +10,6 @@ import { withRouter } from 'react-router';
 import NavBar from './NavBar.jsx';
 import UserSideBar from './UserSideBar.jsx';
 import UserListHeader from './UserListHeader.jsx'
-import Footer from './Footer.jsx'
-import UserList from './UserList.jsx'
 
 class UserProfile extends Component {
 
@@ -20,8 +18,7 @@ class UserProfile extends Component {
   }
 
   componentDidMount() {
-    console.log("here");
-    if(!localStorage.user_name){
+    if(!localStorage.user){
        this.props.history.push({
             pathname: '/login'
           })
@@ -29,26 +26,53 @@ class UserProfile extends Component {
   }
 
   render() {
-    return(
-      <div>
-        <NavBar />
-        <main>
-          {/* <!-- Page Layout here --> */}
-          <div className="row">
-            <UserSideBar />
-              <div className="col s12 m8 l9" id="right">
-                <UserListHeader />
-                <div className="row grid-lists">
-                  <UserList listName={"Movie snacks"} />
-                  <UserList listName={"Movie snacks"} />
-                  <UserList listName={"Movie snacks"} />
-                  <UserList listName={"Movie snacks"} />
+    if(!localStorage.user){
+      console.log("here");
+      return(<div></div>);
+    } else {
+      return(
+        <div>
+          <NavBar />
+          <main>
+            {/* <!-- Page Layout here --> */}
+            <div className="row">
+              <UserSideBar />
+                <div className="col s12 m8 l9" id="right">
+                  <UserListHeader />
+                  <div className="row grid-lists">
+                    <div className="card small blue-cl">
+                      <div className="card-action">
+                        <span className="card-title">Movie snacks</span>
+                        <a href="#"><i className="material-icons right">more_vert</i></a>
+                      </div>
+                    </div>
+                  <div className="card small blue-cl">
+                    <div className="card-action">
+                      <span className="card-title">Barbecue</span>
+                      <a href="#"><i className="material-icons right">more_vert</i></a>
+                    </div>
+                  </div>
+                  <div className="card small blue-cl">
+                    <div className="card-action">
+                      <span className="card-title">Last week</span>
+                      <a href="#"><i className="material-icons right">more_vert</i></a>
+                    </div>
+                  </div>
+                  <div className="card small blue-cl">
+                    <div className="card-action">
+                      <span className="card-title">Birthday party</span>
+                      <a href="#"><i className="material-icons right">more_vert</i></a>
+                    </div>
+                  </div>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-  </div>
+        </main>
+        <footer className="page-footer">
+          <h5 className="icon-footer"><i className="material-icons">shopping_cart</i>Market Buddy</h5>
+          <p className="footer-copy">© 2018 Market Buddy</p>
+        </footer>
+    </div>
 
      )
     }
