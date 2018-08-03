a565c14e63a5c208b89be576f5d4cc9e85f671d5
leor8 <leo442183205@gmail.com>
2018-08-03 14:43:24 -0700

    a bug is fixed where user register or login with no lists will crash the program

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 
 class UserList extends Component{
   render(){
-    console.log('this.props.listName.name');
     return (
       <div className="card small blue-cl">
         <div className="card-action">
